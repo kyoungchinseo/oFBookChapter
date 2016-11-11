65457c4a2019e9527b6a7ca6219ad953d711acfe
kyoungchin <kcseo.acm@gmail.com>
2016-11-11 22:23:08 +0900

    keyboard 입력에 따라 원 크기 변화 기능

--- drawing2D/src/ofApp.cpp
+++ drawing2D/src/ofApp.cpp
@@ -3,6 +3,7 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofSetBackgroundColor(80,80,80);
+    scale = 1;
 }
 
 //--------------------------------------------------------------
@@ -12,22 +13,33 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    float x = 40;
-    float y = 40;
-    float radius = 10;
+    float x = 20*scale;
+    float y = 20*scale;
+    float radius = 10*scale;
     
     for(int i=0;i<100;i++) {
         for(int j=0;j<100;j++) {
             ofNoFill();
             ofSetColor(255, 255, 0);
-            ofDrawCircle(i*20, j*20, 10);
+            ofDrawCircle(i*x, j*y, radius);
         }
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    if (key == 'a') {
+        scale = scale * 1.2;
+    } else if (key == 's') {
+        scale = scale * 0.9;
+    }
+    
+    if (scale > 20) {
+        scale = 20;
+    }
+    if (scale < 1.0) {
+        scale = 1.0;
+    }
 }
 
 //--------------------------------------------------------------
