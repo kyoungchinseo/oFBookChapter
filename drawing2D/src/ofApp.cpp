c876e74e75faf66686412acf89066fb166d20526
kyoungchin <kcseo.acm@gmail.com>
2016-12-20 21:59:47 +0900

    변수 이름 변경

--- drawing2D/src/ofApp.cpp
+++ drawing2D/src/ofApp.cpp
@@ -4,7 +4,7 @@
 void ofApp::setup(){
     ofSetBackgroundColor(80,80,80);
     for(int i=0;i<200;i++) {
-        rectPannel[i].setRectPanel(ofRandom(0,ofGetWidth()),
+        rectPanel[i].setRectPanel(ofRandom(0,ofGetWidth()),
                                 ofRandom(0,ofGetHeight()),
                                 ofRandom(80,120),
                                 ofRandom(80,120),
@@ -23,7 +23,7 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
     for(int i=0;i<200;i++) {
-        rectPannel[i].draw();
+        rectPanel[i].draw();
     }
 }
 
--- drawing2D/src/ofApp.h
+++ drawing2D/src/ofApp.h
@@ -23,7 +23,7 @@ class ofApp : public ofBaseApp{
 		void gotMessage(ofMessage msg);
     
     
-    RectPanel rectPannel[200];
+    RectPanel rectPanel[200];
     
     
 };
